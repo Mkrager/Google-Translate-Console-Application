531fd6b8df61cbc06ee1c5c71d121a4020a6d72d
Smaga Max <smaga.max@gmail.com>
2025-05-24 17:31:04 +0300

    add DisplayLanguages to TranslationConsoleHandler

--- GoogleTranslate.App/Program.cs
+++ GoogleTranslate.App/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using GoogleTranslate.App;
+using GoogleTranslate.App.Contracts;
+using Microsoft.Extensions.DependencyInjection;
+
+Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", @"C:\Users\Smaga\Downloads\light-sunup-460810-q7-a2928fd47886.json");
+
+var services = new ServiceCollection();
+
+services.AddGoogleTranslateServices();
+
+var provider = services.BuildServiceProvider();
+
+var translatorService = provider.GetRequiredService<IGoogleTranslatorService>();
---
+++ GoogleTranslate.App/TranslationConsoleHandler.cs
@@ -0,0 +1,25 @@
+﻿using GoogleTranslate.App.Contracts;
+
+namespace GoogleTranslate.App
+{
+    public class TranslationConsoleHandler
+    {
+        private readonly IGoogleTranslatorService _googleTranslatorService;
+
+        public TranslationConsoleHandler(IGoogleTranslatorService googleTranslatorService)
+        {
+            _googleTranslatorService = googleTranslatorService;
+        }
+
+        public async Task DisplayLanguages()
+        {
+            var languages = await _googleTranslatorService.GetLanguagesList();
+
+            for (int i = 0; i < languages.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. Name: {languages[i].Name}, Code: {languages[i].Code}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
